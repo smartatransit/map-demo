23a8a0e7be52e467a10e7005f6702419eaf50799
Bipol Alam <bipolalam@gmail.com>
2019-11-28 20:20:18 -0500

    reorganizing code a bit

---
+++ public/js/marta.js
@@ -0,0 +1,339 @@
+var gold_line = {
+    "DORAVILLE STATION": [778.5, 733.5],
+    "CHAMBLEE STATION": [742.25, 695],
+    "BROOKHAVEN STATION": [709.25, 661.25],
+    "LENOX STATION": [674.5, 624.75],
+    "LINDBERGH CENTER STATION": [617.375, 554.375],
+    "ARTS CENTER STATION": [543.25, 514],
+    "MIDTOWN STATION": [512.75, 515],
+    "NORTH AVE STATION": [481.125, 514.75],
+    "CIVIC CENTER STATION": [451.25, 515.125],
+    "PEACHTREE CENTER STATION": [409.625, 514.75],
+    "FIVE POINTS STATION": [360.375, 514.25],
+    "GARNETT STATION": [321.75, 490],
+    "WEST END STATION": [265.125, 450.625],
+    "OAKLAND CITY STATION": [231.75, 451],
+    "LAKEWOOD STATION": [198.5, 451.125],
+    "EAST POINT STATION": [165.875, 450.875],
+    "COLLEGE PARK STATION": [115.375, 450.875],
+    "AIRPORT STATION": [32.5, 502.25],
+};
+
+var gold_line_south_ns = {
+    "DORAVILLE STATION": "CHAMBLEE STATION",
+    "CHAMBLEE STATION": "BROOKHAVEN STATION",
+    "BROOKHAVEN STATION": "LENOX STATION",
+    "LENOX STATION": "LINDBERGH CENTER STATION",
+    "LINDBERGH CENTER STATION": "ARTS CENtER STATION",
+    "ARTS CENTER STATION": "MIDTOWN STATION",
+    "MIDTOWN STATION": "NORTH AVE STATION",
+    "NORTH AVE STATION": "CIVIC CENTER STATION",
+    "CIVIC CENTER STATION": "PEACHTREE CENTER STATION",
+    "PEACHTREE CENTER STATION": "FIVE POINTS STATION",
+    "FIVE POINTS STATION": "GARNETT STATION",
+    "GARNETT STATION": "WEST END STATION",
+    "WEST END STATION": "OAKLAND CITY STATION",
+    "OAKLAND CITY STATION": "LAKEWOOD STATION",
+    "LAKEWOOD STATION": "EAST POINT STATION",
+    "EAST POINT STATION": "COLLEGE PARK STATION",
+    "COLLEGE PARK STATION": "AIRPORT STATION",
+    "AIRPORT STATION": "AIRPORT STATION",
+};
+
+var gold_line_north_ns = {
+    "DORAVILLE STATION": "DORAVILLE STATION",
+    "CHAMBLEE STATION": "DORAVILLE STATION",
+    "BROOKHAVEN STATION": "CHAMBLEE STATION",
+    "LENOX STATION": "BROOKHAVEN STATION",
+    "LINDBERGH CENTER STATION": "LENOX STATION",
+    "ARTS CENTER STATION": "LINDBERGH CENTER STATION",
+    "MIDTOWN STATION": "ARTS CENTER STATION",
+    "NORTH AVE STATION": "MIDTOWN STATION",
+    "CIVIC CENTER STATION": "NORTH AVE STATION",
+    "PEACHTREE CENTER STATION": "CIVIC CENTER STATION",
+    "FIVE POINTS STATION": "PEACHTREE CENTER STATION",
+    "GARNETT STATION": "FIVE POINTS STATION",
+    "WEST END STATION": "GARNETT STATION",
+    "OAKLAND CITY STATION": "WEST END STATION",
+    "LAKEWOOD STATION": "OAKLAND CITY STATION",
+    "EAST POINT STATION": "LAKEWOOD STATION",
+    "COLLEGE PARK STATION": "EAST POINT STATION",
+    "AIRPORT STATION": "COLLEGE PARK STATION",
+};
+
+var red_line = {
+    "NORTH SPRINGS STATION": [964.125, 587.125],
+    "SANDY SPRINGS STATION": [909.5, 607.5],
+    "DUNWOODY STATION": [855.375, 631.125],
+    "MEDICAL CENTER STATION": [804.25, 615.75],
+    "BUCKHEAD STATION": [695.875, 586.5],
+    "LINDBERGH CENTER STATION": [617.375, 554.375],
+    "ARTS CENTER STATION": [543.25, 514],
+    "MIDTOWN STATION": [512.75, 515],
+    "NORTH AVE STATION": [481.125, 514.75],
+    "CIVIC CENTER STATION": [451.25, 515.125],
+    "PEACHTREE CENTER STATION": [409.625, 514.75],
+    "FIVE POINTS STATION": [360.375, 514.25],
+    "GARNETT STATION": [321.75, 490],
+    "WEST END STATION": [265.125, 450.625],
+    "OAKLAND CITY STATION": [231.75, 451],
+    "LAKEWOOD STATION": [198.5, 451.125],
+    "EAST POINT STATION": [165.875, 450.875],
+    "COLLEGE PARK STATION": [115.375, 450.875],
+    "AIRPORT STATION": [32.5, 502.25],
+};
+
+var red_line_north_ns = {
+    "NORTH SPRINGS STATION": "NORTH SPRINGS STATION",
+    "SANDY SPRINGS STATION": "NORTH SPRINGS STATION",
+    "DUNWOODY STATION": "SANDY SPRINGS STATION",
+    "MEDICAL CENTER STATION": "DUNWOODY STATION",
+    "BUCKHEAD STATION": "MEDICAL CENTER STATION",
+    "LINDBERGH CENTER STATION": "BUCKHEAD STATION",
+    "ARTS CENTER STATION": "LINDBERGH CENTER STATION",
+    "MIDTOWN STATION": "ARTS CENTER STATION",
+    "NORTH AVE STATION": "MIDTOWN STATION",
+    "CIVIC CENTER STATION": "NORTH AVE STATION",
+    "PEACHTREE CENTER STATION": "CIVIC CENTER STATION",
+    "FIVE POINTS STATION": "PEACHTREE CENTER STATION",
+    "GARNETT STATION": "FIVE POINTS STATION",
+    "WEST END STATION": "GARNETT STATION",
+    "OAKLAND CITY STATION": "WEST END STATION",
+    "LAKEWOOD STATION": "OAKLAND CITY STATION",
+    "EAST POINT STATION": "LAKEWOOD STATION",
+    "COLLEGE PARK STATION": "EAST POINT STATION",
+    "AIRPORT STATION": "COLLEGE PARK STATION",
+};
+
+var red_line_south_ns = {
+    "NORTH SPRINGS STATION": "SANDY SPRINGS STATION",
+    "SANDY SPRINGS STATION": "DUNWOODY STATION",
+    "DUNWOODY STATION": "MEDICAL CENTER STATION",
+    "MEDICAL CENTER STATION": "BUCKHEAD STATION",
+    "BUCKHEAD STATION": "LINDBERGH CENTER STATION",
+    "LINDBERGH CENTER STATION": "ARTS CENtER STATION",
+    "ARTS CENTER STATION": "MIDTOWN STATION",
+    "MIDTOWN STATION": "NORTH AVE STATION",
+    "NORTH AVE STATION": "CIVIC CENTER STATION",
+    "CIVIC CENTER STATION": "PEACHTREE CENTER STATION",
+    "PEACHTREE CENTER STATION": "FIVE POINTS STATION",
+    "FIVE POINTS STATION": "GARNETT STATION",
+    "GARNETT STATION": "WEST END STATION",
+    "WEST END STATION": "OAKLAND CITY STATION",
+    "OAKLAND CITY STATION": "LAKEWOOD STATION",
+    "LAKEWOOD STATION": "EAST POINT STATION",
+    "EAST POINT STATION": "COLLEGE PARK STATION",
+    "COLLEGE PARK STATION": "AIRPORT STATION",
+    "AIRPORT STATION": "AIRPORT STATION",
+};
+
+var L = L;
+var map = null;
+var layerGroup = null
+var poll = true;
+var boardingMap = {};
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    map = L.map('mapid', {
+        crs: L.CRS.Simple
+    });
+    var bounds = [
+        [0, 0],
+        [1000, 1000]
+    ];
+    var image = L.imageOverlay('img/martamap.jpg', bounds).addTo(map);
+    map.fitBounds(bounds);
+    map.on('click', function(ev) {
+        var latlng = map.mouseEventToLatLng(ev.originalEvent);
+        console.log(latlng.lat + ', ' + latlng.lng);
+    });
+    layerGroup = L.layerGroup().addTo(map);
+    pollForTrains();
+});
+
+async function pollForTrains() {
+    for (;;) {
+        if (poll) {
+            console.log("rendering trains")
+            layerGroup.clearLayers();
+            let schedule = await getResponse()
+            renderTrains(schedule);
+            await sleep(5000 * 4);
+        } else {
+            console.log("polling flag turned off");
+            return;
+        }
+    }
+}
+
+function getNextStationMap(line, direction) {
+    switch (line) {
+        case "GOLD":
+            return direction === "S" ? gold_line_south_ns : gold_line_north_ns;
+        case "RED":
+            return direction === "S" ? red_line_south_ns : red_line_north_ns;
+    }
+}
+
+// for a given station, determine if the previous station was recently boarding
+// if it was recently boarding, return that station, otherwise null
+function lastBoarding(station, direction, train_id, line) {
+    switch (line) {
+        case "RED":
+            if (direction === "N") {
+		let prevStation = red_line_south_ns[station]
+                if (boardingMap[buildBoardingKey(prevStation, direction, train_id)]) {
+                    return buildBoardingKey(red_line_south_ns[station], "N", train_id)
+                }
+            } else {
+		let prevStation = red_line_north_ns[station]
+                if (boardingMap[buildBoardingKey(prevStation, direction, train_id)]) {
+                    return buildBoardingKey(red_line_north_ns[station], "S", train_id)
+                }
+            }
+        case "GOLD":
+            if (direction === "N") {
+		let prevStation = gold_line_south_ns[station]
+                if (boardingMap[buildBoardingKey(prevStation, direction, train_id)]) {
+                    return buildBoardingKey(gold_line_south_ns[station], "N", train_id)
+                }
+            } else {
+		let prevStation = gold_line_north_ns[station]
+                if (boardingMap[buildBoardingKey(prevStation, direction, train_id)]) {
+                    return buildBoardingKey(gold_line_north_ns[station], "S", train_id)
+                }
+            }
+    }
+    return null
+}
+
+function getInterpolationCoords(station1, station2, timeToStation2) {
+    if (timeToStation2 <= 0) {
+        return station2
+    }
+    s1x = station1[0]
+    s1y = station1[1]
+    s2x = station2[0]
+    s2y = station2[1]
+    sx = (s2x + s1x) / 2
+    sy = (s2y + s1y) / 2
+    return [sx, sy]
+}
+
+var north_train = L.icon({
+    iconUrl: 'img/train.svg',
+
+    iconSize: [25, 10], // size of the icon
+});
+
+function rotation(direction) {
+    return direction === "S" ? 90 : -90
+}
+
+function addMarker(coords, direction, text) {
+    var sol = L.latLng(coords);
+    L.marker(sol, {
+        icon: north_train,
+        rotationAngle: rotation(direction)
+    }).addTo(layerGroup).bindPopup(text);
+}
+
+function returnNextStation(direction, station) {
+    return direction === "S" ? red_line_south_ns[station] : red_line_north_ns[station];
+}
+
+function returnNextStationCoords(direction, station) {
+    return direction === "S" ? red_line[red_line_south_ns[station]] : red_line[red_line_north_ns[station]];
+}
+
+function returnLastStationCoords(direction, station, line) {
+    switch (line) {
+        case "GOLD":
+	    return direction === "S" ? gold_line[gold_line_north_ns[station]] : gold_line[gold_line_south_ns[station]];
+        case "RED":
+	    return direction === "S" ? red_line[red_line_north_ns[station]] : red_line[red_line_south_ns[station]];
+    }
+    return direction === "S" ? red_line[red_line_north_ns[station]] : red_line[red_line_south_ns[station]];
+}
+
+function buildBoardingKey(station, direction, train_id) {
+    return station + direction + train_id;
+}
+
+function getStationCoordinates(station, line) {
+    switch (line) {
+        case "GOLD":
+            return gold_line[station];
+        case "RED":
+            return red_line[station];
+    }
+}
+
+function renderTrains(schedule) {
+    let currBoarding = {};
+    let boardingList = [];
+    let inTransitList = [];
+    let inTransitTrains = [];
+
+    // record boarding trains
+    //TODO: Why not just use the IDs instead of having a previous boarding or something?
+    for (idx in schedule) {
+        let station = schedule[idx];
+        let station_name = station["STATION"];
+        let direction = station["DIRECTION"];
+        let train_id = station["TRAIN_ID"];
+        let line = station["LINE"];
+        let boardingKey = buildBoardingKey(station_name, direction, train_id);
+        let lastBoardingStation = lastBoarding(station_name, direction, train_id, line);
+        let coords = getStationCoordinates(station_name, line);
+        if (station["WAITING_TIME"] === "Boarding") {
+            if (coords != null) {
+                if (lastBoardingStation) {
+		    console.log("deleting last boarding station because we've made it to the next station: ", lastBoardingStation);
+                    delete boardingMap[lastBoardingStation];
+                }
+                addMarker(coords, station["DIRECTION"], train_id + station["DIRECTION"] + " boarding at " + station["STATION"]);
+
+                currBoarding[boardingKey] = true;
+                boardingMap[boardingKey] = true;
+                boardingList.push(station);
+            } else {
+		console.log("no idea where this station is: ", station);
+	    }
+        } else {
+	    inTransitTrains.push(schedule[idx]);
+	}
+    }
+
+    // record in transit trains
+    for (idx in inTransitTrains) {
+        let station = inTransitTrains[idx];
+        let station_name = station["STATION"]
+        let direction = station["DIRECTION"]
+        let train_id = station["TRAIN_ID"]
+        let line = station["LINE"]
+        let lastBoardingStation = lastBoarding(station_name, direction, train_id, line)
+    	let lastStationCoords = returnLastStationCoords(direction, station_name, line)
+	// if we have a station that recently was boarding, but is not currently boarding, we're in transit
+	// between the previous station, and this one
+        if (lastBoardingStation != null && !(lastBoardingStation in currBoarding)) {
+            newCoords = getInterpolationCoords(lastStationCoords, getStationCoordinates(station_name, line))
+            addMarker(newCoords, direction, train_id + direction + " heading to " + station_name);
+            inTransitList.push(station)
+        }
+    }
+
+    console.log("Trains that are boarding: ", boardingList);
+    console.log("boarding map: ", boardingMap);
+    console.log("Trains that are in transit: ", inTransitList);
+}
+
+async function getResponse() {
+    const response = await fetch('/marta');
+    const myJson = await response.json();
+    return myJson
+}
